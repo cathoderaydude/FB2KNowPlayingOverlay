73356ad0f5c65b18e5e1e7a38fd596225047acdd
Cathode Ray Dude <cathoderaydude@gmail.com>
2025-10-16 21:56:06 -0700

    Added TinyTag support to album images

--- albumart.py
+++ albumart.py
@@ -12,8 +12,16 @@ import shutil
 if len(sys.argv) < 2:
     exit()
 
-# This is the folder passed on the command line
-folder = sys.argv[1]
+# This is the file passed on the command line
+file = sys.argv[1]
+# Get the path to the file
+folder = os.path.dirname(file)
+# And get the folder name for the file
+foldername = os.path.basename(folder)
+
+print ("file" + file)
+print ("folder" + folder)
+print ("foldername " + foldername)
 
 # Get the directory this script lives in so we know where
 # to copy the file. Set the CWD as well, just to be safe.
@@ -21,8 +29,31 @@ abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
 os.chdir(dname)
 
+# First, try TinyTag - if present
+try:
+    from tinytag import TinyTag
+    from PIL import Image
+    import io
+    
+    # Try to parse file
+    tag = TinyTag.get(file, image=True)
+    # Look for cover art
+    if tag.images is not None and tag.images.front_cover is not None:
+        # There is, supposedly, cover art in the file.
+        # PIL should convert it to jpeg automatically, so format doesn't matter
+        Image.open(io.BytesIO(tag.images.front_cover.data)).save("albumart.jpg")
+        
+        # And since we succeeded, go ahead and exit
+        exit()
+except ImportError as e:
+    # If we failed to import a library, tell the user in case they're debugging
+    print ("Error importing library: " + str(e))
+except Exception as e:
+    # If pretty much anything else goes wrong we just want to bail and try to use a file instead.
+    print ("Error trying to use TinyTag: " + str(e))
+
 # Search for each likely filename
-filenames = ["front.jpg", "cover.jpg", "folder.jpg", folder + ".jpg"]
+filenames = ["front.jpg", "cover.jpg", "folder.jpg", os.path.dirname(foldername) + ".jpg"]
 for filename in filenames:
     if(os.path.exists(folder+'\\'+filename)):
         # Copy the file to this folder
