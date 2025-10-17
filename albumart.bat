d10fb126dec8b5f3b6fc7559ef56dcb5cc485c84
Cathode Ray Dude <cathoderaydude@gmail.com>
2025-10-16 18:58:34 -0700

    Improved albumart batch. Added doc images.

--- albumart.bat
+++ albumart.bat
@@ -1,7 +1,17 @@
 SET filename=%~1
-cd "S:\Streaming\Stream assets\FB2KNowPlayingOverlay\"
+cd /D "C:\Code\FB2KNowPlayingOverlay\"
+echo "%filename%" > albumart.txt
 if exist "%filename%\folder.jpg" (	
     copy "%filename%\folder.jpg" albumart.jpg
 ) else (
-    copy noart.jpg albumart.jpg
+    if exist "%filename%\cover.jpg" (
+		copy "%filename%\cover.jpg" albumart.jpg
+	) else (
+		if exist "%filename%\front.jpg" (
+			copy "%filename%\front.jpg" albumart.jpg
+		) else (
+			copy noart.jpg albumart.jpg
+		)
+	)
 )
+pause
